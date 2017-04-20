9fc8c45cd1916601693149eec2504d91fd2408b5
Benjamin Hendricks <hendricksbenjamin@gmail.com>
2017-04-20 18:48:22 +0100

    pod deintegrated

--- Pods/Target Support Files/Pods-RapidDemoTests/Pods-RapidDemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RapidDemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_RapidDemoTests
-@end
--- Pods/Target Support Files/RapidTests/RapidTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RapidTests : NSObject
-@end
-@implementation PodsDummy_RapidTests
-@end
